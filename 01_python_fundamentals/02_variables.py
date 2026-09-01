755c1b5c482395ef0935f77dba157a346e37e0e9
lesson mtileni <mtilenilesson01@gmail.com>
2026-09-01 12:33:06 +0200

    Practice Python variables and data types

---
+++ 01_python_fundamentals/02_variables.py
@@ -0,0 +1,22 @@
+name = "Lesson"
+age = 36
+sql_projects = 11
+study_hours = 3.5
+target_role = "Data Engineer"
+pipeline_success = True
+    
+print("My name is", name)
+print("I am", age, "years old.")
+print("I have completed", sql_projects, "SQL projects.")
+print("I have studied for", study_hours, "hours.")
+print("My target role is", target_role)
+print("Pipeline successful:", pipeline_success)
+
+# Types
+
+print(type(name))  # <class 'str'>
+print(type(age))  # <class 'int'>
+print(type(sql_projects))  # <class 'int'>
+print(type(study_hours))  # <class 'float'>
+print(type(target_role))  # <class 'str'>
+print(type(pipeline_success))  # <class 'bool'>
